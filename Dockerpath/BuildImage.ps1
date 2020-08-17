af686864a40d10156e6a34321e0d53f6f4440168
Stan Wijckmans <stanw@aviobook.aero>
2020-08-17 13:18:02 +0200

    Update version.

--- Dockerpath/BuildImage.ps1
+++ Dockerpath/BuildImage.ps1
@@ -1,4 +1,4 @@
 Write-Host 'Building image'
-docker build --isolation hyperv -t stannieman/audacity-with-asio-builder:1.3.0 .
+docker build --isolation hyperv -t stannieman/audacity-with-asio-builder:1.4.0 .
 Write-Host 'Done building image'
 Read-Host
