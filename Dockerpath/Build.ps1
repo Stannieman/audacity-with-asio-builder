eec09b9bdf8c7c30f192736e56d0e4b61c43a2fc
Stan Wijckmans <stanw@aviobook.aero>
2020-08-17 13:17:56 +0200

    Use Git to download source code and use tar to zip and unzip.

--- Dockerpath/Build.ps1
+++ Dockerpath/Build.ps1
@@ -1,22 +1,22 @@
 $extractedZipDir = 'ExtractedZip'
 
 function DownloadAndExtract ([string]$downloadUrl, [string]$extractedFolderPattern, [string]$destinationName) {
-    Write-Host "----DOWNLOADING $downloadUrl TO FILE file.zip"
-    (New-Object Net.WebClient).DownloadFile($downloadUrl, 'file.zip')
-    Write-Host '----EXTRACTING file.zip'
-    if (Test-Path($extractedZipDir)) {
-        Remove-Item -Recurse -Force extractedZip
-    }
-    New-Item extractedZip -Type Directory
-    Invoke-Expression "& `"$env:EXECUTABLE_7ZIP`" x file.zip * -o$extractedZipDir"
-    Remove-Item file.zip
-    New-Item $destinationName -Type Directory
-    $extractedFolder = $extractedZipDir
-    if ($extractedFolderPattern) {
-        $extractedFolder = (Get-ChildItem $extractedZipDir -Recurse -Filter $extractedFolderPattern).FullName
-    }
-    Write-Host "----RENAMING EXTRACTED FOLDER $extractedFolder TO $destinationName"
-    Move-Item $extractedFolder/* $destinationName/
+	Write-Host "----DOWNLOADING $downloadUrl TO FILE file.zip"
+	(New-Object Net.WebClient).DownloadFile($downloadUrl, 'file.zip')
+	Write-Host '----EXTRACTING file.zip'
+	if (Test-Path($extractedZipDir)) {
+		Remove-Item -Recurse -Force extractedZip
+	}
+	New-Item extractedZip -Type Directory
+	Invoke-Expression "& tar.exe -xf file.zip -C '$extractedZipDir'"
+	Remove-Item file.zip
+	New-Item $destinationName -Type Directory
+	$extractedFolder = $extractedZipDir
+	if ($extractedFolderPattern) {
+		$extractedFolder = (Get-ChildItem $extractedZipDir -Recurse -Filter $extractedFolderPattern).FullName
+	}
+	Write-Host "----RENAMING EXTRACTED FOLDER $extractedFolder TO $destinationName"
+	Move-Item $extractedFolder/* $destinationName/
 }
 
 Write-Host "In order to build Audacity with ASIO support we need to download the Steinberg ASIO SDK.`
@@ -38,16 +38,11 @@ if ($answer -ne 'YES') {
 	exit
 }
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Write-Host "`n`n`n`nDOWNLOADING WXWIDGETS SOURCE"
+Invoke-Expression "& '$env:EXECUTABLE_GIT' clone --recurse-submodules --depth 1 -b 'v$env:WXWIDGETS_VERSION' 'https://github.com/audacity/wxWidgets.git' 'BuildDir/WXWidgets'"
 
-Write-Host "`n`n`n`nDOWNLOADING AND EXTRACTING WXWIDGETS SOURCE"
-DownloadAndExtract -downloadUrl "https://github.com/wxWidgets/wxWidgets/releases/download/v$env:WXWIDGETS_VERSION/wxWidgets-$($env:WXWIDGETS_VERSION).7z" `
--destinationName 'BuildDir/WXWidgets'
-
-Write-Host "`n`n`n`nDOWNLOADING AND EXTRACTING AUDACITY SOURCE"
-DownloadAndExtract -downloadUrl "https://github.com/audacity/audacity/archive/$($env:AUDACITY_COMMIT_HASH).zip" `
--extractedFolderPattern "audacity-$env:AUDACITY_COMMIT_HASH" `
--destinationName 'BuildDir/Audacity'
+Write-Host "`n`n`n`nDOWNLOADING AUDACITY SOURCE"
+Invoke-Expression "& '$env:EXECUTABLE_GIT' clone --recurse-submodules --depth 1 -b '$env:AUDACITY_COMMIT_HASH' 'https://github.com/audacity/audacity.git' 'BuildDir/Audacity'"
 
 Write-Host "`n`n`n`nDOWNLOADING AND EXTRACTING ASIO SDK"
 $asioSdkDownloadUrl = $env:ASIO_SDK_DOWNLOAD_URL
@@ -59,15 +54,15 @@ DownloadAndExtract -downloadUrl $asioSdkDownloadUrl `
 -destinationName 'BuildDir/AsioSdk'
 
 Write-Host "`n`n`n`nBUILDING WXWIDGETS"
-Invoke-Expression "& `"$env:EXECUTABLE_MSBUILD`" BuildDir\WXWidgets\build\msw\wx_vc15.sln /m /t:Build /p:Configuration='DLL Release' /p:BuildInParallel=True /p:PlatformTraget=x86 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0"
+Copy-Item 'BuildDir/WXWidgets/include/wx/msw/setup0.h' 'BuildDir/WXWidgets/include/wx/msw/setup.h'
+Invoke-Expression "& '$env:EXECUTABLE_MSBUILD' BuildDir\WXWidgets\build\msw\wx_vc16.sln /m /t:Build /p:Configuration='DLL Release' /p:BuildInParallel=True /p:PlatformTarget=x86 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0"
 
 Write-Host "`n`n`n`nRESTORING NUGET PACKAGES FOR AUDACITY"
 cd BuildDir/Audacity/win
 Invoke-Expression "& `"$env:EXECUTABLE_NUGET`" restore"
 cd ../../..
-
 Write-Host "`n`n`n`nBUILDING AUDACITY"
-Invoke-Expression "& `"$env:EXECUTABLE_MSBUILD`" BuildDir\Audacity\win\audacity.sln /m /t:Build /p:Configuration=Release /p:BuildInParallel=True /p:PlatformTraget=x86 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0"
+Invoke-Expression "& `"$env:EXECUTABLE_MSBUILD`" BuildDir\Audacity\win\audacity.sln /m /t:Build /p:Configuration=Release /p:BuildInParallel=True /p:PlatformTarget=x86 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0"
 
 Write-Host "`n`n`n`nPREPARING BUILD FOR PACKAGING"
 Get-ChildItem BuildDir/Audacity/win/Release -Filter '*.lib' | Remove-Item
@@ -75,6 +70,7 @@ Remove-Item BuildDir/Audacity/win/Release/Audacity.exp
 Remove-Item BuildDir/Audacity/win/Release/Audacity.iobj
 Remove-Item BuildDir/Audacity/win/Release/Audacity.ipdb
 Remove-Item BuildDir/Audacity/win/Release/Audacity.pdb
+Remove-Item -Recurse BuildDir/Audacity/win/Release/include
 $vcRedistPath = Resolve-Path VSBuildTools/VC/Redist/MSVC/*/x86/Microsoft.VC*.CRT
 Copy-Item $vcRedistPath/* BuildDir/Audacity/win/Release/
 $wxWidgetsVersionInFileName = $env:WXWIDGETS_VERSION -replace '\.', ''
@@ -89,6 +85,8 @@ Rename-Item BuildDir/Audacity/win/Release Audacity
 
 Write-Host "`n`n`n`nCREATING ZIP PACKAGING"
 if (Test-Path('externalvolume/Audacity.zip')) {
-    Remove-Item externalvolume/Audacity.zip
+	Remove-Item externalvolume/Audacity.zip
 }
-Compress-Archive BuildDir/Audacity/win/Audacity externalvolume/Audacity.zip
+
+cd BuildDir/Audacity/win/
+Invoke-Expression "& tar.exe -caf C:\externalvolume/Audacity.zip Audacity"
